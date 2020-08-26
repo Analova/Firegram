2ace06938a21fba4aea96e29463c455b9db68b41
Ana Nikolova <ani.nik19@gmail.com>
2020-08-26 09:03:00 +0200

    connecting to the firebase and working on the file

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import * as firebase from "firebase/app";
+import "firebase/storage";
+import "firebase/firebase";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAz0eKMThl1SdNstu19KycuYRTvPtAq8bw",
+  authDomain: "firegram-3fc00.firebaseapp.com",
+  databaseURL: "https://firegram-3fc00.firebaseio.com",
+  projectId: "firegram-3fc00",
+  storageBucket: "firegram-3fc00.appspot.com",
+  messagingSenderId: "645811729743",
+  appId: "1:645811729743:web:3e62a448348fd3f847c13a",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage;
+const projectFirestore = firebase.firestore();
+
+export { projectStorage, projectFirestore };
